34f9f07250afc3f32c0ef744e377614795f21e40
Rupert De Guzman <rupert@2rmobile.com>
2013-04-24 05:03:27 +1000

    Added support to archive locally, remotely to an archive/backup server and to a hot standby

--- archive.sh
+++ archive.sh
@@ -12,7 +12,7 @@ source_file=$1
 dest_file=$2
 
 archive_locally(){
-   echo "`date`: Archive Locally: cp $source_file $PGARCHIVE_DIR/$dest_file..."
+   echo "`date`: Archive Locally: cp $source_file $PGARCHIVE_DIR/$dest_file"
 
    if [ -f $PGARCHIVE_DIR/$dest_file ]
    then
@@ -24,8 +24,8 @@ archive_locally(){
 }
 
 archive_ssh(){
-   echo "`date`: Archive via Remote SSH: cp $source_file $ARCHIVE_USER@$ARCHIVE_SERVER:$PGARCHIVE_DIR..."
-   scp -P $ARCHIVE_SERVER_SSH_PORT $source_file $ARCHIVE_USER@$ARCHIVE_SERVER:$PGARCHIVE_DIR
+   echo "`date`: $4 via Remote SSH: cp $source_file $2@$3:$PGARCHIVE_DIR"
+   scp -P $1 $source_file $2@$3:$PGARCHIVE_DIR
    if [ $? -ne 0 ];then
       echo "`date`: SCP failed"
       exit 1
@@ -39,11 +39,11 @@ process_archive(){
          archive_locally
          ;;
       1)         
-         archive_ssh
+         archive_ssh $ARCHIVE_SERVER_SSH_PORT $ARCHIVE_USER $ARCHIVE_SERVER "Archive"
          ;;
       2)         
          archive_locally
-         archive_ssh
+         archive_ssh $ARCHIVE_SERVER_SSH_PORT $ARCHIVE_USER $ARCHIVE_SERVER "Archive"
          ;;
       *)
          echo "`date`: Please set the archive mode"
@@ -52,12 +52,21 @@ process_archive(){
    esac
 }
 
+process_standby(){
+   echo $PGSTANDBY_ENABLED
+   if [ $PGSTANDBY_ENABLED = "1" ]
+   then
+      archive_ssh $STANDBY_SERVER_SSH_PORT $STANDBY_USER $STANDBY_SERVER "Standby"
+   fi
+}
+
 if [ -d $PGBACKUP_DIR ] && [ -d $PGARCHIVE_DIR ]
 then
    if [ -f $PGARCHIVE_TRIGGER_FILE ]
    then
       echo "`date`: Found $PGARCHIVE_TRIGGER_FILE. Archiving Active."
       process_archive
+      process_standby
       exit 0
    else
       echo "`date`: Archiving Inactive."
