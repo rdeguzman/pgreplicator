d54b42189f2d50c8ed6a7cd9d3b7874dedada81f
Rupert De Guzman <rupert@2rmobile.com>
2013-04-26 02:09:13 +1000

    Added base_sync to be used for streaming rep

---
+++ base_sync.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# By Rupert
+# Creates a standalone base_backup and WAL segments into 20130423062540.tar.gz
+# The compressed base backup has a directory structure of
+#
+# 20130423062540.tar.gz
+#  pgsql/
+#  archive/ 
+
+# Include directory paths from config file
+DIRNAME=`dirname $0`
+SOURCE=`pwd`
+CONFIG=$SOURCE/$DIRNAME/config
+
+. $CONFIG
+
+echo "Creating $PGARCHIVE_TRIGGER_FILE. Archiving active..."
+touch $PGARCHIVE_TRIGGER_FILE
+
+echo "Starting basebackup..."
+psql -d postgres -U pgsql -c "select pg_start_backup('pgsql_backup'), current_timestamp"
+
+echo "Sync rsync -a $PGDATA/ $STANDBY_USER@$STANDBY_SERVER:$PGDATA/ --exclude postmaster.pid --exclude pg_log"
+rsync -a $PGDATA/ root@$STANDBY_SERVER:$PGDATA/ --exclude postmaster.pid --exclude pg_log
+
+echo "Stopping basebackup..."
+psql -d postgres -U pgsql -c "select pg_stop_backup(), current_timestamp"
+
+echo "Removing $PGARCHIVE_TRIGGER_FILE. Archiving inactive..."
+rm $PGARCHIVE_TRIGGER_FILE
