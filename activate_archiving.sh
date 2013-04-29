3fc550089d5a3a0bd4d6d037e4857fbee3506c07
rupert <rupert@2rmobile.com>
2013-04-30 01:25:43 +1000

    Added activate_archiving

---
+++ activate_archiving.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# By Rupert
+# Archive script to check for existence of archive directory, archive_active file, archive files
+
+# Include directory paths from config file
+DIRNAME=`dirname $0`
+SOURCE=`pwd`
+CONFIG=$SOURCE/$DIRNAME/config
+
+. $CONFIG
+
+touch $PGARCHIVE_TRIGGER_FILE
+chown -Rf pgsql:pgsql $PGARCHIVE_TRIGGER_FILE
