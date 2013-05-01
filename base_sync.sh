34f5f40dc11c924813ad39ee172058b7d458037c
Rupert De Guzman <rupert@2rmobile.com>
2013-05-02 05:36:23 +1000

    Added elapsed time. Rsync now deletes file on slave

--- base_sync.sh
+++ base_sync.sh
@@ -1,4 +1,4 @@
-#!/usr/localbin/bash
+#!/usr/local/bin/bash
 # By Rupert
 # Syncs a base_backup from master to standby
 
@@ -9,20 +9,27 @@ CONFIG=$SOURCE/$DIRNAME/config
 
 . $CONFIG
 
+begin_time=`date +%s`
+
 echo "Creating $PGARCHIVE_TRIGGER_FILE. Archiving active..."
 touch $PGARCHIVE_TRIGGER_FILE
 
 echo "Starting basebackup..."
 psql -d postgres -U pgsql -c "select pg_start_backup('pgsql_backup'), current_timestamp"
 
-echo "Sync rsync -a $PGDATA/ $STANDBY_USER@$STANDBY_SERVER:$PGDATA/ --exclude postmaster.pid --exclude pg_log"
-rsync -a $PGDATA/ root@$STANDBY_SERVER:$PGDATA/ --exclude postmaster.pid --exclude pg_log --exclude pg_xlog
+echo "Sync rsync -e \"ssh -p $STANDBY_SERVER_SSH_PORT\" -a $PGDATA/ root@$STANDBY_SERVER:$PGDATA/ --exclude postmaster.pid --exclude pg_log --exclude pg_xlog"
+rsync -e "ssh -p $STANDBY_SERVER_SSH_PORT" -a $PGDATA/ --del root@$STANDBY_SERVER:$PGDATA/ --exclude postmaster.pid --exclude pg_log --exclude pg_xlog
 
 echo "Stopping basebackup..."
 psql -d postgres -U pgsql -c "select pg_stop_backup(), current_timestamp"
 
 echo "Removing $PGARCHIVE_TRIGGER_FILE. Archiving inactive..."
 rm $PGARCHIVE_TRIGGER_FILE
+
+end_time=`date +%s`
+elapsed_time=`expr $end_time - $begin_time`
+
+echo "Total: $elapsed_time seconds"
 
 echo "--------------IMPORTANT-------------------------"
 echo "Please run setup_standby.sh on the standby server"...
