cc7bb892ca866091d2f8c0d737e6870e58474171
Rupert De Guzman <rupert@2rmobile.com>
2014-03-12 22:54:29 +1100

    Fix typo

--- init.sh
+++ init.sh
@@ -10,16 +10,5 @@ echo "Creating $PGBACKUP_DIR..."
 mkdir -p $PGBACKUP_DIR
 mkdir -p $PGARCHIVE_DIR
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        PGUSER="postgres"
-	PGGROUP="postgres"
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-	PGUSER="pgsql"
-	PGGROUP="pgsql"
-else
-	PGUSER="pgsql"
-	PGGROUP="pgsql"
-fi
-
-echo "Setting permissions to ${PGUSER}:${PGROUGP} for pgsql_backup..."
+echo "Setting permissions to ${PGUSER}:${PGGROUP} for pgsql_backup..."
 chown -Rf ${PGUSER}:${PGGROUP} $PGBACKUP_DIR
